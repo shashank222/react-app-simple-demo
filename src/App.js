c19b5bbaea4ff6cfc81413ba6c6c00132bd4dcb2
Rajesh <rkumar1904@gmail.com>
2020-04-03 12:00:58 +0530

    Project init with basic structure of card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import style from './app.css';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: [],
+      DOB: '',
+      mobile: '',
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  componentDidMount() {
+    const apiURL = 'https://jsonplaceholder.typicode.com/users';
+    fetch(apiURL).then((res) => {
+      return res.json();
+    })
+      .then(result => {
+        const photos = result;
+
+        this.setState({ photos });
+
+      })
+      .catch(err => {
+        console.error('exception:', err);
+      })
+  }
+
+  render() {
+    // console.log('photos from state:', this.state.photos);
+    const { photos } = this.state;
+    return <div>
+      <ul>
+        {photos.map((item, index) => {
+          return <li key={item.id}>
+            <div className="cardName">
+              <h4>{item.name || ''}</h4>
+              <h5>{item.email || ''}</h5>
+              <h5>{item.phone || ''}</h5>
+              <h5>{item.website || ''}</h5>
+            </div>
+          </li>;
+        })}
+        {/* <li>
+          <div className="cardName">
+            <h4>Rahul Rawal</h4>
+            <h5>Backend Developer</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eum magni nostrum nam deserunt reprehenderit exercitationem veniam</p>
+          </div>
+        </li> */}
+      </ul>
     </div>
-  );
+  }
 }
-
 export default App;
